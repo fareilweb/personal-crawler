bfc6c22f7a1e3d11ed8f7feb8c19ece27b8ed993
Luca CIlfone <luca.cilfone@orangedev.it>
2018-12-20 11:18:17 +0100

    Upd

--- core/HttpHelper.php
+++ core/HttpHelper.php
@@ -31,7 +31,7 @@ class RequestResult {
  * HttpHelper class
  * Helper class to make http requests
  */
-class HttpHelper 
+class HttpHelper
 {
     //private $allowed_protocols = array('http', 'https');
     public $user_agent_chrome  = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36';
@@ -40,14 +40,14 @@ class HttpHelper
     public $result = NULL;
 
     /**
-     * Test a Url for Validity    
+     * Test a Url for Validity
      * @param string $uri
      * @return stdClass { boolean status, string message }
      */
     public function isValidUrl($uri = "") {
-        
+
         if(empty($uri)) {
-            return new GenericResult (FALSE, "EMPTY");            
+            return new GenericResult (FALSE, "EMPTY");
         }
 
         $uri_parts = parse_url($uri);
@@ -62,20 +62,20 @@ class HttpHelper
      * @return bool
      */
     public function setUrl($url = "") {
-        if(!empty($url)) {           	    
+        if(!empty($url)) {
             $this->url = $this->escapeUrl($url);
             return TRUE;
-        } 
-        return FALSE;        
+        }
+        return FALSE;
     }
 
-    
+
     public function escapeUrl($uri) {
-	$escUrl = "";	
-	$urlParts = parse_url($uri);	
+	$escUrl = "";
+	$urlParts = parse_url($uri);
 	if (isset($urlParts['scheme'])) {
 	    $escUrl .= $urlParts['scheme']."://";
-	}	
+	}
 	if(isset($urlParts['host'])) {
 	    $escUrl .= $urlParts['host'];
 	}
@@ -85,13 +85,13 @@ class HttpHelper
 	if(isset($urlParts['query'])) {
 	    $escUrl .= "?" . str_replace("%3D", "=", str_replace("%26", "&", urlencode($urlParts['query'])));
 	}
-	if(isset($urlParts['fragment'])) {	
+	if(isset($urlParts['fragment'])) {
 	    $escUrl .= "#" . urlencode($urlParts['fragment']);
 	}
 	return $escUrl;
     }
-    
-    
+
+
     /**
      * Set the user agent to use for the next request
      * @param string $ua_string - User Agent String
@@ -101,36 +101,36 @@ class HttpHelper
         if( !empty($ua_string) ) {
             $this->user_agent = $ua_string;
             return TRUE;
-        } 
-        return FALSE;       
+        }
+        return FALSE;
     }
 
     /**
      * Start request with current settings by cURL
-     * @param boolean $follow_redirect - Follow redirect response or not, default is FALSE
+     * @param bool $follow_redirect - Follow redirect response or not, default is FALSE
      * @return RequestResult instance that contains: $status, $info, $content
      */
     public function makeRequestCurl($follow_redirect = false) {
-        if( empty($this->url) ) { 
+        if( empty($this->url) ) {
             return new RequestResult(false, "EMPTY_OR_INVALID_URL");
         } else {
             $curl = curl_init();
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($curl, CURLOPT_VERBOSE, true);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($curl, CURLOPT_USERAGENT, $this->user_agent_chrome);                        
+            curl_setopt($curl, CURLOPT_USERAGENT, $this->user_agent_chrome);
             curl_setopt($curl, CURLOPT_URL, $this->url);
-            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, $follow_redirect);            
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, $follow_redirect);
             $curl_response = curl_exec($curl);
-            $curl_info = curl_getinfo($curl); 
+            $curl_info = curl_getinfo($curl);
             $this->result = new RequestResult (
-                TRUE, 
-                $curl_info, 
+                TRUE,
+                $curl_info,
                 $curl_response
             );
             curl_close($curl);
             return $this->result;
-        }        
+        }
     }
 
     /**
@@ -138,8 +138,8 @@ class HttpHelper
      * @return RequestResult instance that contains: $status, $info, $content
      */
     public function makeRequestFGC() {
-        if( empty($this->url) ) { 
-            return new RequestResult(FALSE, "EMPTY_OR_INVALID_URL");    
+        if( empty($this->url) ) {
+            return new RequestResult(FALSE, "EMPTY_OR_INVALID_URL");
         } else {
             $file_get_content_result = file_get_contents($this->url);
             $response_header = $http_response_header;
