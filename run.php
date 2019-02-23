8991551c2b17d1b772eed3a1302ffe49a81f87ee
Luca Cilfone - FareilWeb.com <info@fareilweb.com>
2019-02-23 22:04:13 +0100

    upd

--- run.php
+++ run.php
@@ -9,12 +9,13 @@ function AppAutoloader($class_name) {
 
 	$file_found = FALSE;
 	$iteration_index = 0;
-	do {
+	while ($file_found || ($iteration_index) == count($directories)) {
 		$file_path = $directories[$iteration_index] . $class_name . '.php';
 		$file_found = file_exists($file_path);
 		if($file_found) include_once($file_path);
 		$iteration_index++;
-	} while ($file_found || ($iteration_index + 1) == count($directories));
+	}
+
 
 }
 spl_autoload_register('AppAutoloader');
