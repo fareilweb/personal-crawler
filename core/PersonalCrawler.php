c92b1516d9b8bf4a999b07aec84a1bd229f002de
Luca Cilfone - FareilWeb.com <info@fareilweb.com>
2019-02-24 21:11:03 +0100

    Update

--- core/PersonalCrawler.php
+++ core/PersonalCrawler.php
@@ -6,20 +6,16 @@ class PersonalCrawler
 {
 	private $args = [];
 
-	function __construct(Array $argv = [])
+	function __construct()
 	{
-		$this->Initialize($argv);
+
 	}
 
 	function Initialize(Array $argv)
 	{
 		unset($argv[0]); // Remove first argoument (script name)
 		$this->args = array_values($argv); // Re-Index argouments array
-
-
 		print_r($this->args);
 	}
-
-	
 
 }
--- run.php
+++ run.php
@@ -3,25 +3,35 @@
 /* App Autoloader */
 function AppAutoloader($class_name) {
 
-    $directories = [
+    $dirs = [
 		__DIR__ . '/core/'
 	];
 
 	$file_found = FALSE;
-	$iteration_index = 0;
-	while ($file_found || ($iteration_index) == count($directories)) {
-		$file_path = $directories[$iteration_index] . $class_name . '.php';
+	$index = 0;
+	$dirs_count = count($dirs);
+
+	while ($file_found == FALSE && $index < $dirs_count)
+	{
+
+		$file_path = $dirs[$index] . $class_name . '.php'; echo $file_path . " --- ";
 		$file_found = file_exists($file_path);
-		if($file_found) include_once($file_path);
-		$iteration_index++;
+		if($file_found) {
+			include_once($file_path);
+		}
+
+		echo $index . ' - ' . $dirs_count . ' | ';
+
+		$index++;
+
+		if($index == 10) {
+			exit;
+		}
 	}
-
-
 }
 spl_autoload_register('AppAutoloader');
 
+
 $pc = new PersonalCrawler();
-
 $pc->Initialize($argv);
-
 ?>
