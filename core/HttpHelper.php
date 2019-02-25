3f773ad7c831dc70ca99b24b663d13e7bc61c3a3
Luca Cilfone - FareilWeb.com <info@fareilweb.com>
2019-02-25 13:48:17 +0100

    up

--- core/HttpHelper.php
+++ core/HttpHelper.php
@@ -4,15 +4,42 @@
  */
 class HttpHelper
 {
-
+	/**
+	 * $_userAgents
+	 * @var [array]
+	 */
 	private $_userAgents = [
 		'chrome' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
 		'firefox' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0'
 	];
 
 
-	function __construct()
-	{
+	/**
+	 * [The starting url]
+	 * @var [string] - the url from wich start crawling
+	 */
+	private $_url;
+	public function SetUrl($url) {
+		$this->_url = $url;
+	}
+	public function GetUrl() {
+		return $_url;
+	}
+
+	/**
+	 * [The current user agent]
+	 * @var [string] - user agent string to use inside http request
+	 */
+	private $_userAgent;
+	public function SetUserAgent($user_agent_key) {
+		$this->_userAgent = $user_agent_key;
+	}
+	public function GetUserAgent() {
+		return $this->_userAgent;
+	}
+
+
+	function __construct() {
 
 	}
 
