98b73c79b04bd8131f380b366a7a6da93f0f5b61
Luca CIlfone <luca.cilfone@orangedev.it>
2019-02-28 18:10:16 +0100

    update with localizations

--- core/managers/LocalizationManager.php
+++ core/managers/LocalizationManager.php
@@ -7,11 +7,25 @@ class LocalizationManager
      */
     private $current_language_code;
 
+    private $current_localization_data;
+
     public function __construct()
     {
         $this->current_language_code = LANGUAGE_CODE;
+
+
+        $localizations_file_path = PATH_LOCALIZATIONS . DIRECTORY_SEPARATOR . LANGUAGE_CODE . ".json";
+        if(file_exists($localizations_file_path)) 
+        {
+            $localizations_json_string = file_get_contents( $localizations_file_path );            
+            $this->current_localization_data = json_decode( $localizations_json_string, FALSE )
+        }
+        
     }
 
-    
+    public function GetString(string $stringId) : string
+    {
+        return "";
+    }  
 
 }
